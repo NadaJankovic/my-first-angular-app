1acfc454fcce54d834e9de0daebaf6ef84f5e002
nada.jankovic <nada.jankovic@leversys.com>
2022-01-17 11:16:04 +0100

    code refactoring; added tests for edit item

--- src/cypress/integration/test-app.spec.js
+++ src/cypress/integration/test-app.spec.js
@@ -3,8 +3,8 @@
 const { url } = require("inspector");
 
 
-describe('Testing while ulr included /homePage', () => {
-  beforeEach(()=> {
+describe('Testing while on Home Pge', () => {
+  beforeEach(() => {
     cy.visit('/homePage');
   });
 
@@ -23,7 +23,7 @@ describe('Testing while ulr included /homePage', () => {
   });
 
   it('should find Add New List Item  anchor tag  and click on it', () => {
-     cy.get(':nth-child(2) > a')
+    cy.get(':nth-child(2) > a')
       .click()
       .should('be.visible')
       .should('contain.text', 'Add New List Item')
@@ -32,7 +32,7 @@ describe('Testing while ulr included /homePage', () => {
   });
 });
 
-describe('Testing when url include /addNewItem', () => {
+describe('Testing while on Add New List item', () => {
   beforeEach(() => {
     cy.visit('/addNewItem')
       .get('kendo-formfield')
@@ -45,7 +45,7 @@ describe('Testing when url include /addNewItem', () => {
       .type('Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.')
       .should('include.value', 'Lorem ipsum dolor sit amet')
   });
-  it('should find and type in title and content and click return button', () => {
+  it('should type in title and content and click return button', () => {
     cy
       .get('.return-button')
       .should('have.class', 'return-button')
@@ -60,31 +60,116 @@ describe('Testing when url include /addNewItem', () => {
       .should('be.focused')
       .click()
       .url().should('include', '/homePage')
-
-      .get('table>tr').eq(0)
-      .should('have.length', 1)
-      .click()
-      .wait(2000)
-      .url()
-      .url().should('include', '/items')
-      .get('.item-details-title')
-      .should('include.text', 'Title')
-      .get('.item-details-content')
-      .should('include.text', 'Lorem ipsum dolor sit amet')
-      .get('button')
-      .should('include.text', 'Return')
-      .click()
-      .url().should('include', '/homePage')
-
-      .get('.delete-btn')
-      .should('include.text', 'Delete')
-      .focus()
-      .should('be.focused')
-      .wait(2000)
-      .click()
-      .url().should('include', '/homePage')
-      .get('h1')
-      .should('include.text', 'To do list is currently empty')
   });
 
+  describe('After form submission', () => {
+    beforeEach(() => {
+      cy.get('.input-save-button')
+        .focus()
+        .should('have.class', 'input-save-button')
+        .should('be.focused')
+        .click()
+        .url().should('include', '/homePage')
+    });
+
+    it('should find first item in the list and click on it and click on Return button when item details is opened ', () => {
+      cy.get('table>tr').eq(0)
+        .should('have.length', 1)
+        .click()
+        .url()
+        .url().should('include', '/items')
+        .get('.item-details-title')
+        .should('include.text', 'Title')
+        .get('.item-details-content')
+        .should('include.text', 'Lorem ipsum dolor sit amet')
+        .get('button')
+        .should('include.text', 'Return')
+        .click()
+        .url().should('include', '/homePage')
+    });
+
+    it('should find and delete first item in the list', () => {
+      cy.get('.delete-btn')
+        .should('include.text', 'Delete')
+        .focus()
+        .should('be.focused')
+        .wait(2000)
+        .click()
+        .url().should('include', '/homePage')
+        .get('h1')
+        .should('include.text', 'To do list is currently empty')
+    });
+
+    it('should find and edit first item in the list then cick on return button', () => {
+      cy.get('.edit-btn')
+        .should('include.text', 'Edit')
+        .focus()
+        .should('be.focused')
+        .click()
+        .url()
+        .should('include', '/editItem')
+
+        .get('kendo-formfield')
+        .find('#title')
+        .clear()
+        .wait(2000)
+        .get('#kendo-error-2')
+        .should('be.visible')
+        .should('contain.text', 'Title and Content required')
+        .get('#kendo-error-3')
+        .should('not.exist')
+        
+        .get('kendo-formfield')
+        .find('#title')
+        .type('New Title')
+        .should('have.value', 'New Title')
+        .get('kendo-formfield')
+        .find('#content')
+        .clear()
+        .wait(2000)
+        .get('#kendo-error-3')
+        .should('be.visible')
+        .should('contain.text', 'Title and Content required')
+        .get('kendo-formfield')
+        .find('#content')
+        .type('Test content.')
+        .should('include.value', 'Test content')
+
+        .get('.return-button')
+        .should('contain.text', 'Return')
+        .click()
+        .url()
+        .should('include', '/homePage')
+    });
+
+    it('should find and edit first item in the list then cick on submit button', () => {
+      cy.get('.edit-btn')
+        .should('include.text', 'Edit')
+        .focus()
+        .should('be.focused')
+        .click()
+        .url()
+        .should('include', '/editItem')
+
+        .get('kendo-formfield')
+        .find('#title')
+        .clear()
+        .type('New Title')
+        .should('have.value', 'New Title')
+        .get('kendo-formfield')
+        .find('#content')
+        .clear()
+        .type('Test content.')
+        .should('include.value', 'Test content')
+
+        .get('.input-save-button')
+        .should('contain.text', 'Submit')
+        .click()
+        .url()
+        .should('include', '/homePage')
+
+        .get('table>tr').eq(0)
+        .should('contain.text', 'New Title')
+    });
+  });
 });
